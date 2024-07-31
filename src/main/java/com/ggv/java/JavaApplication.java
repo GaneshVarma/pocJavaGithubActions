f710e0d886d742a07066ed4b21d95681f67c1833
Gadiraju <Ganesh.Gadiraju@bakerhughes.com>
2024-07-31 11:23:49 +0530

    Commit

---
+++ src/main/java/com/ggv/java/JavaApplication.java
@@ -0,0 +1,13 @@
+package com.ggv.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+
+}
