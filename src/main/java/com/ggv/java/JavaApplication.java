2c26aaa65823ef9118431e7e6cd1edfb55e25a50
Ganesh Gadiraju <ganeshvarma@gmail.com>
2024-07-31 11:25:57 +0530

    Update JavaApplication.java

--- src/main/java/com/ggv/java/JavaApplication.java
+++ src/main/java/com/ggv/java/JavaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("<<<<<<<<< -------- SPRING BOOT APPLICATION DEMO  ----------->>>>>>>>");
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
